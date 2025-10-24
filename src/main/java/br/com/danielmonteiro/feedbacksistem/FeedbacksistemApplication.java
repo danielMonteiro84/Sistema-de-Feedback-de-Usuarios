3c3252d378cc4a134a7122172eab972bc5a89abf
daniel <danielchsan@gmail.com>
2025-10-24 13:56:26 -0300

    Commit inicial do projeto `feedbacksistem`

---
+++ src/main/java/br/com/danielmonteiro/feedbacksistem/FeedbacksistemApplication.java
@@ -0,0 +1,13 @@
+package br.com.danielmonteiro.feedbacksistem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbacksistemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbacksistemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/danielmonteiro/feedbacksistem/FeedbacksistemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danielmonteiro.feedbacksistem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbacksistemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
