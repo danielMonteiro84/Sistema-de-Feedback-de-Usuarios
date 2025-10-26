ce625587859cad047b4e34cf6af929620274303e
daniel <danielchsan@gmail.com>
2025-10-26 09:20:47 -0300

    adicionando diretório de tratamento de Exceptions

---
+++ src/main/java/br/com/danielmonteiro/feedbacksistem/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,12 @@
+package br.com.danielmonteiro.feedbacksistem.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+
+    private String message;
+    private String field;
+}
